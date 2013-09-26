056061e41eeef0914622e25d65708dd9a5220393
chenya <chen.yachin@gmail.com>
2013-09-26 14:52:34 +0300

    Add a Fibonacci function

--- GitTest/TestGit.cpp
+++ GitTest/TestGit.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace  std;
 
+int fib(int n);
+
+int fib(int n)
+{
+	return (n < 3) ? 1 : (fib(n-1) + fib(n - 2)) ;
+}
+
+
 int main()
 {
 	cout << "Hello World" << endl;
+	
+	cout << "FIB 10:" << fib(10) << endl;
+
 	return 0;
 }
